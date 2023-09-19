4e5f841f2dc96af770ba3395d3bf82cb1e14c7a7
Markus Wildgruber <M-Wildgruber@users.noreply.github.com>
2023-09-19 15:13:23 +0200

    Added samples on collations

---
+++ src/Samples.M2/C2/TextFilterCollation.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using Samples.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.M2.C2
+{
+    internal class TextFilterCollation : IMovieSample
+    {
+        public async Task RunAsync(IMongoCollection<Movie> movies, IMongoDatabase db, Configuration config)
+        {
+            var searchText = "tHe gOdFaThEr";
+            var binaryResult = await (await movies.FindAsync(x => x.Title == searchText)).ToListAsync();
+            PrintResult(binaryResult, "Binary comparison");
+            
+            var collEn = new Collation("en");
+            var enResult = await (await movies.FindAsync(x => x.Title == searchText, new FindOptions<Movie, Movie>() { Collation = collEn })).ToListAsync();
+            PrintResult(enResult, "Comparison using collation EN");
+            var collEnLowerStrength = new Collation("en", strength: CollationStrength.Secondary);
+            var enLowerStrengthResult = await (await movies.FindAsync(x => x.Title == searchText, new FindOptions<Movie, Movie>() { Collation = collEnLowerStrength })).ToListAsync();
+            PrintResult(enLowerStrengthResult, "Comparison using collation EN and strength 2");
+        }
+
+        private void PrintResult(IEnumerable<Movie> movies, string text)
+        {
+            var output = movies.Any() ? "a" : "no";
+            Console.WriteLine($"{text} found {output} movie");
+        }
+    }
+}
---
+++ src/Samples.M2/C2/TextSortCollation.cs
@@ -0,0 +1,76 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Samples.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.M2.C2
+{
+    internal class TextSortCollation : ITestDatabaseSample
+    {
+        private readonly ConsoleHelper _consoleHelper;
+
+        public TextSortCollation(ConsoleHelper consoleHelper)
+        {
+            _consoleHelper = consoleHelper;
+        }
+
+        public async Task RunAsync(IMongoDatabase db, Configuration config)
+        {
+            var coll = db.GetCollection<TestDocument>("m2c2_text_sort_collation");
+            await PrepareDataAsync(coll);
+
+            Console.WriteLine("WITHOUT COLLATION");
+            await LoadAndPrintDocsAsync(coll, Collation.Simple);
+            _consoleHelper.Separator();
+
+            Console.WriteLine("WITH COLLATION");
+            var collation = new Collation("en", numericOrdering: true);
+            await LoadAndPrintDocsAsync(coll, collation);
+        }
+
+        private async Task LoadAndPrintDocsAsync(IMongoCollection<TestDocument> coll, Collation collation)
+        {
+            var sort = Builders<TestDocument>.Sort.Ascending(x => x.Version);
+            var options = new FindOptions<TestDocument, TestDocument>()
+            {
+                Sort = sort,
+                Collation = collation
+            };
+            var docs = (await coll.FindAsync(Builders<TestDocument>.Filter.Empty, options)).ToEnumerable();
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Version);
+            }
+        }
+
+        private async Task PrepareDataAsync(IMongoCollection<TestDocument> coll)
+        {
+            await coll.DeleteManyAsync(Builders<TestDocument>.Filter.Empty);
+            var docs = Enumerable.Range(1, 10).SelectMany(x => new TestDocument[]
+            {
+                new() { Version = $"{x}.{x % 2}.{x % 4}"},
+                new() { Version = $"{x}.{x % 3}.{x % 6}"},
+                new() { Version = $"{x}.{x % 4}.{x % 8}"},
+                new() { Version = $"{x * 2}.{(x * 2) % 2}.{(x * 2) % 4}"},
+                new() { Version = $"{x * 2}.{(x * 2) % 3}.{(x * 2) % 6}"},
+                new() { Version = $"{x * 2}.{(x * 2) % 4}.{(x * 2) % 8}"},
+                new() { Version = $"{x * 100}.{(x * 100) % 2}.{(x * 100) % 4}"},
+                new() { Version = $"{x * 100}.{(x * 100) % 3}.{(x * 100) % 6}"},
+                new() { Version = $"{x * 100}.{(x * 100) % 4}.{(x * 100) % 8}"},
+            });
+            await coll.InsertManyAsync(docs);
+        }
+
+        public class TestDocument
+        {
+            [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+            public string Id { get; set; } = string.Empty;
+
+            public string Version { get; set; } = string.Empty;
+        }
+    }
+}
