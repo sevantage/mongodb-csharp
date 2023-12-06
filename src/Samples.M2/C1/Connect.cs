0bb9b83cc256c658c270236dd251aa80a58fc2ef
Markus Wildgruber <M-Wildgruber@users.noreply.github.com>
2023-12-06 16:56:54 +0100

    Added connect sample

---
+++ src/Samples.M2/C1/Connect.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Samples.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.M2.C1
+{
+    internal class Connect : ISampleWithConfig
+    {
+        public async Task RunAsync(Configuration config)
+        {
+            var client = new MongoClient(config.ConnectionString);
+            var db = client.GetDatabase(config.SampleMoviesDatabaseName);
+            var coll = db.GetCollection<BsonDocument>("movies");
+            var allDocs = (await coll.FindAsync(FilterDefinition<BsonDocument>.Empty, new FindOptions<BsonDocument, BsonDocument>() { Limit = 10 })).ToEnumerable();
+            foreach (var doc in allDocs)
+            {
+                Console.WriteLine($"{doc["title"].AsString.PadRight(80)} | {doc["year"]}");
+            }
+        }
+    }
+}
