eb889d41ffca7a57ef296647f785f60f1915d1be
Markus Wildgruber <M-Wildgruber@users.noreply.github.com>
2023-12-07 15:26:07 +0100

    Added sample on conventions

---
+++ src/Samples.M4/C3/Conventions.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using Samples.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.M4.C3
+{
+    public class Conventions : ITestDatabaseSample
+    {
+        public async Task RunAsync(IMongoDatabase db, Configuration config)
+        {
+            var pack = new ConventionPack();
+            pack.Add(new CamelCaseElementNameConvention());
+            ConventionRegistry.Register(
+                "Camel Case Convention",
+                pack,
+                t => true);
+
+            var coll = db.GetCollection<TestDocument>("m4c3_conventions");
+            await coll.DeleteManyAsync(FilterDefinition<TestDocument>.Empty);
+
+            await coll.InsertOneAsync(new TestDocument() { Name = "My name", Value = 123 });
+        }
+
+        [BsonNoId]
+        public class TestDocument
+        {
+            public string Name { get; set; } = string.Empty;
+
+            public int Value { get; set; }
+        }
+    }
+}
